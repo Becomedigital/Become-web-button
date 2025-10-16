c43df5db9ad59f9beb582122d4daf4e6ba3b85de
Roberto Paternina <23266204+paternina@users.noreply.github.com>
2025-10-16 12:34:27 -0500

    refactor: signupHost, new events to example

--- src/become-button.js
+++ src/become-button.js
@@ -51,7 +51,7 @@ export default class BecomeButton extends LitElement {
     this.disabled = true;
     this.loading = true;
     this.apiHost = "https://api.become.com";
-    this.signupHost = "http://localhost:5173/";
+    this.signupHost = "https://onboarding-v2.svi.becomedigital.net";
     [this.language] = navigator.language.split("-");
     this.metadata = null;
 
