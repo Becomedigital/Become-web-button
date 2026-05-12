c4c794ab1afe1cd6f5853d4e422c80d0be9e1f74
Roberto Paternina <23266204+paternina@users.noreply.github.com>
2026-05-12 15:03:37 -0500

    fix: host

--- src/become-button.js
+++ src/become-button.js
@@ -53,7 +53,7 @@ export default class BecomeButton extends LitElement {
     this.disabled = true;
     this.loading = true;
     this.apiHost = "https://api.become.com";
-    this.signupHost = "https://onboarding-demo3.svi.becomedigital.net";
+    this.signupHost = "https://onboarding.svi.becomedigital.net";
     [this.language] = navigator.language.split("-");
     this.metadata = null;
 
